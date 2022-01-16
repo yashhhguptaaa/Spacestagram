4b9fc4e1380eda7d5530bbad84d527a6ebc0426f
yash gupta <yash3144@gmail.com>
2022-01-16 16:35:17 +0530

    Add like and copy button

--- components/card.js
+++ components/card.js
@@ -1,32 +1,70 @@
+import { useState } from "react";
+import { AiOutlineCalendar, AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import { RiFileCopyLine } from "react-icons/ri";
+import { BsCheckAll } from "react-icons/bs";
 import Link from "next/link";
 import Image from "next/image";
 import styles from "../styles/Card.module.css";
-import cls from 'classnames';
+import cls from "classnames";
 
 const Card = (props) => {
-    console.log({props})
+  const [isLiked, setClick] = useState(false);
+  const [linkCopied, setLinkCopied] = useState(false);
   return (
-    <div >
-        <div className={cls("glass", styles.container)}>
-          
-          <div >
-            <Image
-              className={styles.cardImage}
-              src={props.imgUrl}
-              width={307}
-              height={256}
-            />
-          </div>
-
-          <div >
-            <h2 className={styles.cardHeader}>{props.name}</h2>
-          </div>
-
-          <div >
-            <h2 >{props.content}</h2>
-          </div>
-
+    <div>
+      <div className={cls("glass", styles.container)}>
+        <div>
+          <Image
+            className={styles.cardImage}
+            src={props.imgUrl}
+            width={307}
+            height={256}
+          />
         </div>
+
+        <div>
+          <h2 className={styles.cardHeader}>{props.name}</h2>
+        </div>
+        <div className={styles.iconWrapper}>
+          <Image src="/icon/calendar.svg" width="24" height="24" />
+          <p className={styles.text}>{props.date}</p>
+        </div>
+        <div className={styles.cardContent}>
+          <p>{props.content}</p>
+        </div>
+
+        <div>
+          <button className={styles.likeButton} onClick={() => {}}>
+            {isLiked ? (
+              <AiFillHeart
+                size={24}
+                color="red"
+                style={{
+                  backgroundColor: "hsla(0, 0%, 100%, 0.4)",
+                  marginTop: -2,
+                }}
+              />
+            ) : (
+              <AiOutlineHeart
+                size={24}
+                //   color={theme === "light" ? "#333" : "#eee"}
+                style={{ marginTop: -2 }}
+              />
+            )}
+          </button>
+          <button
+            className={styles.copyButton}
+            aria-label="copy button"
+            onClick={() => console.log("Copy button click")}
+          >
+            {linkCopied ? (
+              <BsCheckAll size={24} color="green" />
+            ) : (
+              <RiFileCopyLine />
+            )}
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
